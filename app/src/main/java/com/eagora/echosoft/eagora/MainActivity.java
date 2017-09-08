f17897587ec17c45b01aaceaeb513387c9ab0f17
rShigenari <r.shigenari@gmail.com>
2017-09-08 20:06:19 -0300

    Uso do GraphRequest/ tentativa de retorno da info

--- app/src/main/java/com/eagora/echosoft/eagora/MainActivity.java
+++ app/src/main/java/com/eagora/echosoft/eagora/MainActivity.java
@@ -4,14 +4,21 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.FacebookSdk;
+import com.facebook.GraphRequest;
+import com.facebook.GraphResponse;
 import com.facebook.Profile;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+
+import org.json.JSONObject;
+
+import java.util.Arrays;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -34,23 +41,43 @@ public class MainActivity extends AppCompatActivity {
         txtStatus = (TextView)findViewById(R.id.txtStatus);
         login_button = (LoginButton)findViewById(R.id.login_button);
         callbackManager = CallbackManager.Factory.create();
-
+        login_button.setReadPermissions(Arrays.asList(
+                "public_profile", "email", "user_birthday", "user_friends"));
         login_button.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @Override
             public void onSuccess(LoginResult loginResult) {
-                txtStatus.setText("Login sucess! \n" + "User ID: "+
+                login_button.setReadPermissions("email,public_profile");
+                txtStatus.setText("Login sucess! \n" + "User ID: " +
                         loginResult.getAccessToken().getUserId()
-                        +"\n"+ "Last Refresh:\n " +loginResult.getAccessToken().getLastRefresh() + "\n");
+                        + "\n" + "Last Refresh:\n " + loginResult.getAccessToken().getLastRefresh() + "\n");
 
-                if(Profile.getCurrentProfile()!=null){
-                    txtStatus.setText("Login sucess! \n" + "User ID: "+
+                if (Profile.getCurrentProfile() != null) {
+                    txtStatus.setText("Login sucess! \n" + "User ID: " +
                             loginResult.getAccessToken().getUserId()
-                            +"\n"+ "Last Refresh:\n " +loginResult.getAccessToken().getLastRefresh() + "\n" +
+                            + "\n" + "Last Refresh:\n " + loginResult.getAccessToken().getLastRefresh() + "\n" +
                             "nome: \n" + Profile.getCurrentProfile().getName());
                 }
+                GraphRequest.newMeRequest(
+                        loginResult.getAccessToken(), new GraphRequest.GraphJSONObjectCallback() {
+                            @Override
+                            public void onCompleted(JSONObject me, GraphResponse response) {
+                                if (response.getError() != null) {
+                                    // handle error
+                                } else {
+                                    String email = me.optString("email");
+                                    String id = me.optString("id");
+                                    String name = me.optString("name");
+                                    Toast.makeText(getApplicationContext(),
+
+                                            email + " / " + name,
+
+                                            Toast.LENGTH_LONG).show();
+                                    // send email and id to your web server
+                                }
+                            }
+                        }).executeAsync();
+
             }
-
-
             @Override
             public void onCancel() {
             }
