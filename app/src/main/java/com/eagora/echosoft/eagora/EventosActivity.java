349f37931fce9d95256c459e608b5fbc1d01c614
Gerson <gersongoulartjnr@gmail.com>
2017-10-13 17:36:17 -0300

    Mostra TODOS os eventos a partir de uma localização. Capa do evento clicável, redireciona para o evento do Facebook. Melhorias a serem feitas furamente:  - Informar usuário que o processo está acontecendo. - Informar posição do GPS e raio (atualmente está uma posição fixa do centro de SJC) - Melhoria no tempo de execução.

--- app/src/main/java/com/eagora/echosoft/eagora/EventosActivity.java
+++ app/src/main/java/com/eagora/echosoft/eagora/EventosActivity.java
@@ -1,16 +1,23 @@
 package com.eagora.echosoft.eagora;
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.method.LinkMovementMethod;
+import android.text.util.Linkify;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.androidquery.AQuery;
 import com.eagora.echosoft.eagora.Facebook.AcessoGraphFacebook;
@@ -18,6 +25,7 @@ import com.facebook.AccessToken;
 import com.facebook.FacebookSdk;
 import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
+import com.google.firebase.auth.FirebaseAuth;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -25,8 +33,12 @@ import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 public class EventosActivity extends AppCompatActivity {
+
+    Button btnBuscarEventos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,16 +48,161 @@ public class EventosActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         InitializeControls();
+
     }
 
-    private void  InitializeControls() {
+    private void  InitializeControls(){
+
+        btnBuscarEventos = (Button)findViewById(R.id.btnBuscarEventos);
+        btnBuscarEventos.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        eventosProximos();
+                    }
+                }
+        );
+    }
+
+
+    private void eventosProximos(){
+        AcessoGraphFacebook acesso = new AcessoGraphFacebook();
+        double latitude = -23.1994939;
+        double longitude = -45.8918287;
+        double raio = 1000;
+
+        //Mapeia o LinearLayout
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linearEventos);
+
+        List<JSONObject> listaEventos = acesso.procurarEventos(latitude,longitude,raio);
+        for(int i=0;i<listaEventos.size();i++){
+            try {
+                    //Para cada resultado de evento, cria os elementos
+                    //Padrão +10,+20,..,+40 para os id's
+                    TextView txtNomeEvento = new TextView(this);
+                    txtNomeEvento.setId(i);
+                    TextView txtLocal = new TextView(this);
+                    txtLocal.setId(i+100);
+                    ImageView imgEvento = new ImageView(this);
+                    imgEvento.setId(i+200);
+                    TextView txtHorario = new TextView(this);
+                    txtHorario.setId(i+300);
+                    TextView txtEndereco = new TextView(this);
+                    txtEndereco.setId(i+400);
+
+
+                    //Objeto pai
+                    JSONObject dados = listaEventos.get(i);
+
+                    //Nome do Evento
+                    txtNomeEvento.setText("Evento " +  dados.get("name").toString());
+
+                    //Converter Data - JSON
+                    String data = dados.get("start_time").toString().substring(0,10);
+                    SimpleDateFormat formatoJSON = new SimpleDateFormat("yyyy-MM-dd");
+                    Date dataJSON = formatoJSON.parse(data);
+                    SimpleDateFormat formatoDesejado = new SimpleDateFormat("dd/MM/yyyy");
+
+                    //Data e Horário
+                    String horario = dados.get("start_time").toString().substring(11,16);
+                    txtHorario.setText("Data e Horário: " + formatoDesejado.format(dataJSON).toString() + " - " + horario );
+
+
+                    //Cover (capa) da imagem do evento do Facebook
+                    JSONObject cover = dados.getJSONObject("cover");
+                    AQuery aq=new AQuery(this);
+                    aq.id(imgEvento).image(cover.get("source").toString());
+
+                    //Cria a URL
+                    final String url = "https://www.facebook.com/events/" + dados.get("id") + "/";
+
+                    //Seta a imagemview como clicável
+                    imgEvento.setClickable(true);
+
+                    //Abre o link do evento
+                    imgEvento.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setAction(Intent.ACTION_VIEW);
+                            intent.addCategory(Intent.CATEGORY_BROWSABLE);
+                            intent.setData(Uri.parse(url));
+                            startActivity(intent);
+                        }
+                    });
+
+
+                    //Local e Endereço
+                    JSONObject local = dados.getJSONObject("place");
+                    txtLocal.setText("Local: " + local.get("name").toString() );
+                    JSONObject localizacao = local.getJSONObject("location");
+                    txtEndereco.setText("Endereço: " + localizacao.get("street").toString() + ", " +
+                            localizacao.get("zip").toString() + "\n" +
+                            localizacao.get("city").toString() + ", " + localizacao.get("state").toString());
+
+                    //Itens do layout adicionados
+                    linear.addView(imgEvento);
+                    linear.addView(txtNomeEvento);
+                    linear.addView(txtHorario);
+                    linear.addView(txtLocal);
+                    linear.addView(txtEndereco);
+            }
+            catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private void locais(){
+        //Mapeia o LinearLayout
+        LinearLayout linear = (LinearLayout)findViewById(R.id.linearEventos);
+
+        //Acessando Método para Pesquisar Eventos
+        AcessoGraphFacebook acesso = new AcessoGraphFacebook();
+        double latitude = -23.1994939;
+        double longitude = -45.8918287;
+        double raio = 500;
+        GraphResponse response = acesso.locais(latitude,longitude,raio);
+        try {
+            //Criando um array do objeto pai "data"
+            JSONArray ar = response.getJSONObject().getJSONArray("data");
+            for(int i=0;i<ar.length();i++){
+
+                //Para cada resultado de evento, cria os elementos
+                //Padrão +10,+20,..,+40 para os id's
+                TextView txtNomeLocal = new TextView(this);
+                txtNomeLocal.setId(i);
+                TextView txtCategoria = new TextView(this);
+                txtCategoria.setId(i+10);
+
+                //Objeto pai
+                JSONObject dados = ar.getJSONObject(i);
+
+                //Nome e Categoria do Local
+                txtNomeLocal.setText("Local: " +  dados.get("name").toString());
+                txtCategoria.setText("Categoria: " + dados.get("category").toString());
+
+
+                //Itens do layout adicionados
+                linear.addView(txtNomeLocal);
+                linear.addView(txtCategoria);
+
+            }
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+    private void  eventos() {
 
        //Mapeia o LinearLayout
         LinearLayout linear = (LinearLayout)findViewById(R.id.linearEventos);
 
         //Acessando Método para Pesquisar Eventos
         AcessoGraphFacebook acesso = new AcessoGraphFacebook();
-        GraphResponse response = acesso.eventosTeste();
+        GraphResponse response = acesso.eventos("");
         try {
             //Criando um array do objeto pai "data"
             JSONArray ar = response.getJSONObject().getJSONArray("data");
--- app/src/main/java/com/eagora/echosoft/eagora/Facebook/AcessoGraphFacebook.java
+++ app/src/main/java/com/eagora/echosoft/eagora/Facebook/AcessoGraphFacebook.java
@@ -6,6 +6,12 @@ import com.facebook.GraphRequest;
 import com.facebook.GraphResponse;
 import android.os.Bundle;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 /**
  * Created by junior on 11/10/17.
@@ -21,10 +27,50 @@ public class AcessoGraphFacebook {
     //Variável de retorno das consultas da API Graph do Facebook
     GraphResponse responseRetorno;
 
-    public GraphResponse eventosTeste(){
+
+    public List<JSONObject> procurarEventos(double latitude, double longitude, double raio){
+        responseRetorno = locais(latitude,longitude,raio);
+        List<String> listaLocais = new ArrayList<String>();
+        try {
+            JSONArray ar = responseRetorno.getJSONObject().getJSONArray("data");
+            for (int i = 0; i < ar.length(); i++) {
+                JSONObject dados = ar.getJSONObject(i);
+                listaLocais.add(i,dados.get("id").toString());
+            }
+        }        catch(Exception e){
+            e.printStackTrace();
+        }
+
+
+        List<JSONObject> listaEventos = new ArrayList<JSONObject>();
+        int tam=0;
+        for(int i=0; i<listaLocais.size();i++){
+            try {
+                 responseRetorno = eventos(listaLocais.get(i));
+
+                JSONArray ar = responseRetorno.getJSONObject().getJSONArray("data");
+                if(ar.length()>0){
+                    for(int j=0;j<ar.length();j++){
+                        JSONObject dados = ar.getJSONObject(j);
+                        listaEventos.add(j,dados);
+                    }
+                }
+
+            }        catch(Exception e){
+                e.printStackTrace();
+            }
+
+        }
+        return listaEventos;
+
+    }
+
+    public GraphResponse eventos(String id){
+        // "/542158045980461/events"
+        String queryId = "/"+ id +"/events";
         final GraphRequest request = GraphRequest.newGraphPathRequest(
                 accessToken,
-                "/542158045980461/events",
+                queryId,
                 new GraphRequest.Callback() {
                     @Override
                     public void onCompleted(GraphResponse response) {
@@ -32,8 +78,44 @@ public class AcessoGraphFacebook {
                     }
                 });
         Bundle parameters = new Bundle();
-        parameters.putString("fields", "name,start_time,cover{source},place{name,location{city,street,zip,state}}");
+        parameters.putString("fields", "id,name,start_time,cover{source},place{name,location{city,street,zip,state}}");
         parameters.putString("time_filter", "upcoming");
+        request.setParameters(parameters);
+
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                GraphResponse gResponse = request.executeAndWait();
+            }
+        });
+        t.start();
+
+        try {
+            t.join();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return responseRetorno;
+    }
+
+    public GraphResponse locais(double latitude, double longitude, double raio){
+        final GraphRequest request = GraphRequest.newGraphPathRequest(
+                accessToken,
+                "/search",
+                new GraphRequest.Callback() {
+                    @Override
+                    public void onCompleted(GraphResponse response) {
+                        responseRetorno = response;
+                    }
+                });
+        Bundle parameters = new Bundle();
+        parameters.putString("q", "*");
+        parameters.putString("type", "place");
+        String coordenadas = String.valueOf(latitude) +","+String.valueOf(longitude);
+        parameters.putString("center", coordenadas);
+        parameters.putString("distance", String.valueOf(raio));
+        parameters.putString("fields", "name,category");
+        parameters.putString("limit", "500"); //Limite de 500 respostas
         request.setParameters(parameters);
 
         Thread t = new Thread(new Runnable() {
