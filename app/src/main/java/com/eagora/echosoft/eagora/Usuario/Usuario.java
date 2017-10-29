c5474b013dc42f6e6600439bff1b4d03407e2082
Gerson <gersongoulartjnr@gmail.com>
2017-10-28 23:53:13 -0200

    Edição de Cadastro (sem layout correto)

--- app/src/main/java/com/eagora/echosoft/eagora/Usuario/Usuario.java
+++ app/src/main/java/com/eagora/echosoft/eagora/Usuario/Usuario.java
@@ -1,5 +1,7 @@
 package com.eagora.echosoft.eagora.Usuario;
 
+import java.util.List;
+
 /**
  * Created by dhanielmazini on 20/10/17.
  */
@@ -11,6 +13,7 @@ public class Usuario {
     public String email;
     private String uid;
     private String senha;
+    private List<String> perfil;
 
     public Usuario() {
 
@@ -44,6 +47,10 @@ public class Usuario {
         return senha;
     }
 
+    public List<String> getPerfil() {
+        return perfil;
+    }
+
     public void setSenha(String senha) {
         this.senha = senha;
     }
@@ -62,5 +69,9 @@ public class Usuario {
 
     public void setUid(String uid) {
         this.uid = uid;
+    }
+
+    public void setPerfil(List<String> perfil) {
+        this.perfil = perfil;
     }
 }
