f238c8d4dd6db548f3bee2974fbff1fb5746e722
Gerson <gersongoulartjnr@gmail.com>
2017-10-28 23:53:13 -0200

    Edição de Cadastro (sem layout correto)

--- app/src/main/java/com/eagora/echosoft/eagora/CadastroEditarActivity.java
+++ app/src/main/java/com/eagora/echosoft/eagora/CadastroEditarActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,17 +23,28 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class CadastroEditarActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
 
-    TextView uidTxt, nameText, emailTxt;
-    Button excluirbtn, btnnome, btnemail, senhabtn, editPerfil;
-    EditText editName, editMAil, editpw;
-    private FirebaseAuth mAuth;
+
+    EditText txtNome,txtSobrenome,txtEmail,txtNovaSenha,txtSenhaConfirmar;
+    Button btnEditar,btnExcluir;
+    CheckBox cbVidaNoturna,cbTrabalho,cbPaisagem,cbGastro,cbFamilia,cbCultural,cbCompras,cbAventura;
+    List<String> perfis;
+    DatabaseReference mDatabase;
+    Usuario usuario;
+    String senha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,30 +52,25 @@ public class CadastroEditarActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cadastro_editar);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
-        setContentView(R.layout.activity_cadastro_editar);
-        uidTxt = (TextView) findViewById(R.id.uidTxt);
-        emailTxt = (TextView) findViewById(R.id.emailTxt);
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        excluirbtn = (Button)findViewById(R.id.excluirConta);
-        editName = (EditText)findViewById(R.id.editName);
-        nameText = (TextView)findViewById(R.id.name);
-        btnnome = (Button)findViewById(R.id.btnnome);
-        editMAil = (EditText) findViewById(R.id.editMail);
-        btnemail = (Button)findViewById(R.id.btnemail);
-        senhabtn = (Button)findViewById(R.id.senhabtn);
-        editpw = (EditText)findViewById(R.id.editpw);
-        editPerfil = (Button)findViewById(R.id.editPerfil);
 
+        txtNome = (EditText) findViewById(R.id.txtNome);
+        txtSobrenome = (EditText) findViewById(R.id.txtSobrenome);
+        txtEmail = (EditText) findViewById(R.id.txtEmail);
+        txtNovaSenha = (EditText) findViewById(R.id.txtNovaSenha);
+        txtSenhaConfirmar = (EditText) findViewById(R.id.txtSenhaConfirmar);
+        btnEditar = (Button) findViewById(R.id.btnEditar);
+        cbVidaNoturna = (CheckBox) findViewById(R.id.cbVidaNoturna);
+        cbTrabalho = (CheckBox) findViewById(R.id.cbTrabalho);
+        cbPaisagem = (CheckBox) findViewById(R.id.cbPaisagem);
+        cbGastro = (CheckBox) findViewById(R.id.cbGastro);
+        cbFamilia = (CheckBox) findViewById(R.id.cbFamilia);
+        cbCultural = (CheckBox) findViewById(R.id.cbCultural);
+        cbCompras = (CheckBox) findViewById(R.id.cbCompras);
+        cbAventura = (CheckBox) findViewById(R.id.cbAventura);
+        btnExcluir = (Button) findViewById(R.id.btnExcluir);
 
         setSupportActionBar(toolbar);
 
@@ -71,142 +78,100 @@ public class CadastroEditarActivity extends AppCompatActivity {
         DatabaseReference userRef = FirebaseDatabase.getInstance().getReference();
         DataSnapshot ds;
 
+        usuario = new Usuario(user.getDisplayName(), " ", user.getEmail(), user.getUid(), "");
 
+        //Assíncrono
+        mDatabase = FirebaseDatabase.getInstance().getReference("usuarios").child(user.getUid());
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Usuario usu = dataSnapshot.getValue(Usuario.class);
+                    senha = usu.getSenha();
+                    txtNome.setText(usu.getNome());
+                    txtSobrenome.setText(usu.getSobrenome());
+                    txtEmail.setText(usu.getEmail());
+                    List<String> perfis;
+                    perfis = usu.getPerfil();
 
-        if (user != null) {
-            Usuario usuario = new Usuario(user.getDisplayName(), " ", user.getEmail(), user.getUid(), "");
-            // Name, email address, and profile photo Url
-            String uid = usuario.getUid();
-            String email = usuario.getEmail();
-
-            // Check if user's email is verified
-            boolean emailVerified = user.isEmailVerified();
-
-            emailTxt.setText(email);
-
-            uidTxt.setText(uid);
-
-            btnnome.setOnClickListener(
-
-                    new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-
-                            UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
-                                    .setDisplayName(String.valueOf(editName))
-                                    .build();
-
-                            user.updateProfile(profileUpdates)
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(CadastroEditarActivity.this, "Nome atualizado",
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                        }
+                    //Checkbox do tipo de perfil
+                    for(int i=0;i<perfis.size();i++){
+                        if(perfis.get(i).toString().equals("Aventura")) cbAventura.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Compras")) cbCompras.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Cultural")) cbCultural.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Família")) cbFamilia.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Gastronômico")) cbGastro.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Paisagem")) cbPaisagem.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Trabalho")) cbTrabalho.setChecked(true);
+                        else if(perfis.get(i).toString().equals("Vida Noturna")) cbVidaNoturna.setChecked(true);
                     }
-            );
-            nameText.setText(user.getDisplayName());
 
-            btnemail.setOnClickListener(
+                }
+            }
 
-                    new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-                            FirebaseUser user2 = FirebaseAuth.getInstance().getCurrentUser();
-                            user2.updateEmail(String.valueOf(editMAil)).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                @Override
-                                public void onComplete(@NonNull Task<Void> task) {
-                                    if (task.isSuccessful()) {
-                                        Toast.makeText(CadastroEditarActivity.this, "Email atualizado",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
-                                    else {
-                                        Toast.makeText(CadastroEditarActivity.this, "Falha na atualização",
-                                                Toast.LENGTH_SHORT).show();
-                                    }
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        });
+
+        btnExcluir.setOnClickListener(
+
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+                        mDatabase.removeValue();
+                        FirebaseAuth.getInstance().signOut();
+                        Intent login = new Intent(getApplicationContext(), LoginActivity.class);
+                        startActivity(login);
+                    }
+
+                }
+        );
+
+
+        btnEditar.setOnClickListener(
+
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view) {
+
+                        List<String> novosPerfils = new ArrayList<String>();
+                        if (cbAventura.isChecked()) novosPerfils.add("Aventura");
+                        if (cbCompras.isChecked()) novosPerfils.add("Compras");
+                        if (cbCultural.isChecked()) novosPerfils.add("Cultural");
+                        if (cbFamilia.isChecked()) novosPerfils.add("Família");
+                        if (cbGastro.isChecked()) novosPerfils.add("Gastronômico");
+                        if (cbPaisagem.isChecked()) novosPerfils.add("Paisagem");
+                        if (cbTrabalho.isChecked()) novosPerfils.add("Trabalho");
+                        if (cbVidaNoturna.isChecked()) novosPerfils.add("Vida Noturna");
+
+                        if (novosPerfils.size() <= 3) {
+
+                            if (txtSenhaConfirmar.getText().toString().equals(senha)) {
+
+                                Map<String, Object> hopperUpdates = new HashMap<String, Object>();
+                                hopperUpdates.put("nome", txtNome.getText().toString());
+                                hopperUpdates.put("sobrenome", txtSobrenome.getText().toString());
+                                hopperUpdates.put("email", txtEmail.getText().toString());
+                                if (!txtNovaSenha.getText().toString().isEmpty()) {
+                                    hopperUpdates.put("senha", txtNovaSenha.getText().toString());
                                 }
-                            });
+
+                                hopperUpdates.put("perfil", novosPerfils);
+
+                                mDatabase.updateChildren(hopperUpdates);
+                                Toast.makeText(getApplicationContext(), "Dados atualizados!", Toast.LENGTH_LONG).show();
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Senha incorreta", Toast.LENGTH_LONG).show();
+                            }
+                        } else {
+                            Toast.makeText(getApplicationContext(), "Selecione no máximo 3 perfis", Toast.LENGTH_LONG).show();
                         }
                     }
-            );
-
-            excluirbtn.setOnClickListener(
-                    new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-                            user.delete()
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(CadastroEditarActivity.this, "Conta Excluída",
-                                                        Toast.LENGTH_SHORT).show();
-                                                FirebaseAuth.getInstance().signOut();
-                                                Intent login = new Intent(getApplicationContext(), LoginActivity.class);
-                                                startActivity(login);
-                                            }
-                                        }
-                                    });
-                        }
-                    }
-            );
-            editPerfil.setOnClickListener(
-                    new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-                            Intent intentPerfil = new Intent(getApplicationContext(), PerfilViajanteActivity.class);
-                            startActivity(intentPerfil);
-                        }
-                    }
-            );
-
-            senhabtn.setOnClickListener(
-
-                    new View.OnClickListener(){
-                        @Override
-                        public void onClick(View view){
-
-                            user.updatePassword(String.valueOf(editpw))
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(CadastroEditarActivity.this, "Senha alterada!",
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                            else{
-                                                Toast.makeText(CadastroEditarActivity.this, "Falha na alteração",
-                                                        Toast.LENGTH_SHORT).show();
-                                            }
-                                        }
-                                    });
-                        }
-                    }
-            );
-
-            // Get auth credentials from the user for re-authentication. The example below shows
-// email and password credentials but there are multiple possible providers,
-// such as GoogleAuthProvider or FacebookAuthProvider.
-            AuthCredential credential = EmailAuthProvider
-                    .getCredential(user.getEmail(), String.valueOf(user.updatePassword(String.valueOf(editpw))));
-
-// Prompt the user to re-provide their sign-in credentials
-            user.reauthenticate(credential)
-                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            Toast.makeText(CadastroEditarActivity.this, "Reautenticado",
-                                    Toast.LENGTH_SHORT).show();
-                        }
-                    });
 
 
+                }
+        );
 
-        }
+
     }
 
 
